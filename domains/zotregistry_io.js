00218d087863a11539c4760e250e2515d8a3f158
Christopher LIJLENSTOLPE <cdl@asgaard.org>
2022-09-21 12:57:44 -0700

    added docs as a cname for github pages

--- domains/zotregistry_io.js
+++ domains/zotregistry_io.js
@@ -3,10 +3,15 @@ D("zotregistry.io", REG_HEX,
 // website
 //
   CNAME('www', 'project-zot.github.io.'),
+  CNAME('docs', 'project-zot.github.io.'),
 //
 // PR test
 // 
  TXT('@', 'zot is cool'),
+//
+// Validation codes
+//
+TXT('_github-pages-challenge-project-zot.docs', 'acee8e1be67bd9361fb30314926ee7'),
 //
 // Naked A and AAAA records
 //
@@ -17,6 +22,4 @@ D("zotregistry.io", REG_HEX,
   AAAA('@', '2606:50c0:8000::153'),
   AAAA('@', '2606:50c0:8001::153'),
   AAAA('@', '2606:50c0:8002::153'),
-  AAAA('@', '2606:50c0:8003::153'),
-  TXT('_github-pages-challenge-project-zot.docs', 'acee8e1be67bd9361fb30314926ee7'),
-  CNAME('docs', 'project-zot.github.io.')
+  AAAA('@', '2606:50c0:8003::153')
